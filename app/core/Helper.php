4779c83636aa230fb94b3b52287bb9d9af00efdf
samocs <samoforgi@gmail.com>
2026-06-18 00:01:37 +0200

    e

--- app/core/Helper.php
+++ app/core/Helper.php
@@ -8,6 +8,10 @@ class Helper
         $page = ucfirst(basename($script, '.php'));
         return 'Wallpaper Hub - ' . $page;
     }
+    public static function e(string $s): string
+    {
+        return htmlspecialchars($s, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+    }
 }
 class Redirect{
     public static function redirect(string $url):void{
